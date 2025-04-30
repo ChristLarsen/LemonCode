686fe1fc392e13f45908d9259eb22fabbd32a729
ChristLarsen <c.larsen@mayoral.com>
2025-04-30 16:52:09 +0200

    Cambios motor -> ui

--- modulo8.3-aplicacion-memoria/prueba-concepto-4/src/main.ts
+++ modulo8.3-aplicacion-memoria/prueba-concepto-4/src/main.ts
@@ -1,5 +1,5 @@
 // Prueba de concepto 4: Mostrar segunda imagen y volver las dos cartas.
-import { iniciaJuego } from "./motor";
+import { iniciaJuego } from "./ui";
 
 // Al cargar el DOM, inicia el juego
 document.addEventListener("DOMContentLoaded", iniciaJuego);
