c45f8cbc6ccc030c84b5152ba23f93148244e37a
ChristLarsen <c.larsen@mayoral.com>
2025-05-16 11:53:07 +0200

    Se agrega algun caso en main.ts

--- modulo9.2-clave-fuerte/src/main.ts
+++ modulo9.2-clave-fuerte/src/main.ts
@@ -1,4 +1,6 @@
-import { } from "./clavefuerte/index";
+import { commonPasswords, validarClave } from "./clavefuerte/index";
 
+// Comprueba la clave
+console.log(validarClave("christian", "Abc!L700aaa", commonPasswords));
 
-
+console.log(validarClave("christian", "Ab1!LchristiaNaaa", commonPasswords));
